e7da2de4be5805ee482501fdaeb216c0f22bab16
Kelvin Freeman <freeman.181@wright.edu>
2024-04-10 23:38:42 -0400

    Making changes to the webpage in the are of audio recording. Moving to Node js to take advantage of the wav audio recording library. Will use the wav library to record the audio and then sent the data to the server through a POSt message. Created a littel python server script to test out the audio recording on this ubuntu local host before puting the code on the EC2 server.

--- webPage/record-live-audio.js
+++ webPage/record-live-audio.js
@@ -10,6 +10,13 @@
 // MediaRecorder.stop -> stop recording (this will generate a blob of data)
 // URL.createObjectURL -> to create a URL from a blob, which we can use as audio src
 
+import {MediaRecorder, register} from 'extendable-media-recorder';
+import {connect} from 'extendable-media-recorder-wav-encoder';
+
+//let mediaRecorder = null;
+//let audioBlobs = [];
+//let capturedStream = null;
+
 var recordButton, stopButton, recorder;
 
 window.onload = function () {
@@ -24,7 +31,7 @@ window.onload = function () {
     recordButton.disabled = false;
     recordButton.addEventListener('click', startRecording);
     stopButton.addEventListener('click', stopRecording);
-    recorder = new MediaRecorder(stream);
+    recorder = new MediaRecorder(stream, {mimeType: 'audio/wav'});
 
     // listen to dataavailable, which gets triggered whenever we have
     // an audio blob available
@@ -43,6 +50,13 @@ function stopRecording() {
   recordButton.disabled = false;
   stopButton.disabled = true;
 
+    mediaRecorder.addEventListener('stop', () => {
+      const mimeType = mediaRecorder.mimeType;
+      const audioBlob = new Blob(audioBlobs, { type: mimeType });
+	  
+      resolve(audioBlob);
+    });
+
   // Stopping the recorder will eventually trigger the `dataavailable` event and we can complete the recording process
   recorder.stop();
 }
@@ -52,4 +66,28 @@ function onRecordingReady(e) {
   // e.data contains a blob representing the recording
   audio.src = URL.createObjectURL(e.data);
   audio.play();
+  uploadBlob(audioBlob, 'wav');
+}
+
+/**
+ * Uploads audio blob to your server
+ * @params {Blob} audioBlob - The audio blob data
+ * @params {string} fileType - 'mp3' or 'wav'
+ * @return {Promise<object>)
+ */
+function uploadBlob(audioBlob, fileType) {
+  const formData = new FormData();
+  formData.append('audio_data', audioBlob, 'file');
+  formData.append('type', fileType || 'wav');
+
+  // Your server endpoint to upload audio:
+  const apiUrl = "http://localhost:8000";
+
+  const response = fetch(apiUrl, {
+    method: 'POST',
+    cache: 'no-cache',
+    body: formData
+  });
+
+  return response.json();
 }
