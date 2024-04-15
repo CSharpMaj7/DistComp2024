c82abe0c9a417c270bd3e2bbddb7254cb9245302
Kelvin <freeman.181@wright.edu>
2024-04-15 13:07:05 -0400

    got the audio files passed using local host, now will try to do the same test on the web server

--- webPage/record-live-audio.js
+++ webPage/record-live-audio.js
@@ -1,10 +1,22 @@
 //Code References 
 //https://github.com/mozdevs/MediaRecorder-examples/blob/gh-pages/filter-and-record-live-audio.html
 //https://stackoverflow.com/questions/12407778/connecting-to-tcp-socket-from-browser-using-javascript
-//
+//https://franzeus.medium.com/record-audio-in-js-and-upload-as-wav-or-mp3-file-to-your-backend-1a2f35dea7e8
+//import {MediaRecorder, register} from 'extendable-media-recorder';
+//import {connect} from 'extendable-media-recorder-wav-encoder';
 
+let mediaRecorder = null;
+let audioBlobs = [];
+let capturedStream = null;
 var recordButton, stopButton, recorder;
 var xhr=new XMLHttpRequest();
+
+// Register the extendable-media-recorder-wav-encoder
+//async connect() {
+//  await register(await connect());
+//}
+
+
 //let audioBlobs = [];
 
 window.onload = function () {
@@ -74,42 +86,15 @@ async function uploadBlob(audioBlob, fileType) {
   formData.append('audio_data', audioBlob, 'file');
   formData.append('type', fileType || 'ogg');
 
-	navigator.tcpPermission.requestPermission({remoteAddress:"ec2-34-230-82-177.compute-1.amazonaws.com", remotePort:8000}).then(
-	  () => {
-	    // Permission was granted
-	    // Create a new TCP client socket and connect to remote host
-	    var mySocket = new TCPSocket("ec2-34-230-82-177.compute-1.amazonaws.com", 8000);
-
-	    // Send data to server
-	    mySocket.writeable.write("Hello World").then(
-		() => {
-
-		    // Data sent sucessfully, wait for response
-		    console.log("Data has been sent to server");
-		    mySocket.readable.getReader().read().then(
-			({ value, done }) => {
-			    if (!done) {
-				// Response received, log it:
-				console.log("Data received from server:" + value);
-			    }
-
-			    // Close the TCP connection
-			    mySocket.close();
-			}
-		    );
-		},
-		e => console.error("Sending error: ", e)
-	    );
-	  }
-	);	
+	
   // Your server endpoint to upload audio:
-  //const apiUrl = "http://localhost:8000";
+  const apiUrl = "http://localhost:8000";
 
-  //const response = await fetch(apiUrl, {
-  //  method: 'POST',
-  //  cache: 'no-cache',
-  //  body: formData
-  //:wait});
+  const response = await fetch(apiUrl, {
+    method: 'POST',
+    cache: 'no-cache',
+    body: formData
+    });
 
-  return response.json();
+  return response;
 }
