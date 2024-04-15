53738fc29eb31a10dab58f8d1054a5411e6a9a8e
Kelvin Freeman <freeman.181@wright.edu>
2024-04-15 03:16:18 +0000

    Have reached my stopping point. Was able to get the ec2 instance running, ssl certs installed, the google-speech-to-text environment mostly working and setup a few expeimental python http servers on port 8000. However was not able to put all of the pieces together. Did not know that javascript required https therefore I was slowed down and tried to re-write some of the code. But most of the piece are somewhat developed but need more time.

--- backendPython/speechApi.py
+++ backendPython/speechApi.py
@@ -1,7 +1,11 @@
-from google.cloud import speech
+# code references
+# https://cloud.google.com/speech-to-text/docs/transcribe-client-libraries
+# https://realpython.com/python-sockets/
 
+#from google.cloud import speech
 
-
+import socket
+'''
 def run_quickstart() -> speech.RecognizeResponse:
     # Instantiates a client
     client = speech.SpeechClient()
@@ -23,5 +27,20 @@ def run_quickstart() -> speech.RecognizeResponse:
     for result in response.results:
         print(f"Transcript: {result.alternatives[0].transcript}")
 
-run_quickstart()
+#run_quickstart()
+'''
+HOST = "0.0.0.0"  # Standard loopback interface address (localhost)
+PORT = 8000  # Port to listen on (non-privileged ports are > 1023)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
 
--- webPage/record-live-audio.js
+++ webPage/record-live-audio.js
@@ -74,34 +74,34 @@ async function uploadBlob(audioBlob, fileType) {
   formData.append('audio_data', audioBlob, 'file');
   formData.append('type', fileType || 'ogg');
 
-navigator.tcpPermission.requestPermission({remoteAddress:"ec2-34-230-82-177.compute-1.amazonaws.com", remotePort:8000}).then(
-  () => {
-    // Permission was granted
-    // Create a new TCP client socket and connect to remote host
-    var mySocket = new TCPSocket("ec2-34-230-82-177.compute-1.amazonaws.com", 8000);
+	navigator.tcpPermission.requestPermission({remoteAddress:"ec2-34-230-82-177.compute-1.amazonaws.com", remotePort:8000}).then(
+	  () => {
+	    // Permission was granted
+	    // Create a new TCP client socket and connect to remote host
+	    var mySocket = new TCPSocket("ec2-34-230-82-177.compute-1.amazonaws.com", 8000);
 
-    // Send data to server
-    mySocket.writeable.write("Hello World").then(
-        () => {
+	    // Send data to server
+	    mySocket.writeable.write("Hello World").then(
+		() => {
 
-            // Data sent sucessfully, wait for response
-            console.log("Data has been sent to server");
-            mySocket.readable.getReader().read().then(
-                ({ value, done }) => {
-                    if (!done) {
-                        // Response received, log it:
-                        console.log("Data received from server:" + value);
-                    }
+		    // Data sent sucessfully, wait for response
+		    console.log("Data has been sent to server");
+		    mySocket.readable.getReader().read().then(
+			({ value, done }) => {
+			    if (!done) {
+				// Response received, log it:
+				console.log("Data received from server:" + value);
+			    }
 
-                    // Close the TCP connection
-                    mySocket.close();
-                }
-            );
-        },
-        e => console.error("Sending error: ", e)
-    );
-  }
-
+			    // Close the TCP connection
+			    mySocket.close();
+			}
+		    );
+		},
+		e => console.error("Sending error: ", e)
+	    );
+	  }
+	);	
   // Your server endpoint to upload audio:
   //const apiUrl = "http://localhost:8000";
 
