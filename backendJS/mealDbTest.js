48b2e0182e8c7a14cbf48b03ba8488d76e85a86a
Kelvin <freeman.181@wright.edu>
2024-04-16 15:37:20 -0400

    Able to filter for recipes through api and to get the meal instructions

--- backendJS/mealDbTest.js
+++ backendJS/mealDbTest.js
@@ -3,46 +3,36 @@
  
  async function mealDbTest(){
 
-        //the mealdb api key is 
-    
+        //the mealdb api key is 9973533
+        var json; 
         //try{
         //const response = await fetch(`https://www.themealdb.com/api/json/v1/I-5VDXKMUMB45R/search.php?s=${encodeURIComponent(foodList.join(",")}`);
         //const response = await fetch(`https://www.themealdb.com/api/json/v1/I-5VDXKMUMB45R/search.php?s=chicken_breast,tomato,garlic`).then(
-        const response = await fetch(`https://www.themealdb.com/api/json/v2/9973533/filter.php?i=chicken_breast,garlic,salt`).then(
+        const response = await fetch(`https://www.themealdb.com/api/json/v2/9973533/filter.php?i=chicken,garlic,salt`).then(
             function(response) {
               if (response.status !== 200) {
                 console.log('Problem in fetching');
-                return;
+                return null;
               }
-              response.text().then(function(data) {
-                
-                var json = JSON.parse(data)
-                
-                console.log(data);
-                console.log(json.meals[0].idMeal);
-                
-                
-              });
-              console.log(typeof response);
-            }) 
-              
-        
-        const response2 = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=52772`).then(
+              return response.text().then(function(data) {              
+                json = JSON.parse(data)
+                return json;          
+              });               
+        })     
+             
+        console.log(response.meals[0].idMeal); 
+        const response2 = await fetch(`https://www.themealdb.com/api/json/v2/9973533/lookup.php?i=` + response.meals[0].idMeal ).then(
             function(response2) {
               if (response2.status !== 200) {
                 console.log('Problem in fetching');
                 return;
               }
-              response2.text().then(function(data) {
-                console.log(data);
+              return response2.text().then(function(data) {
+                json = JSON.parse(data)
+                return json
               });
             })   
-            
-
-        //} catch (e) {
-         // exception handling
-        // }   
-             
+        console.log(response2.meals[0].strMeal);     
         
 }
 
